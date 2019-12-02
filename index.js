058f24afedfb6b3ed8fd8040e805536b0f347e2e
kibens <j.tusamba@gmail.com>
2019-12-02 11:42:49 +0100

    add all files

---
+++ index.js
@@ -0,0 +1,22 @@
+/* MODULES */
+const express = require('express')
+const path = require('path')
+const app = express()
+
+/* GLOBAL CONSTANTES */
+const PORT = process.env.PORT || 5000
+
+/* TEMPLATE ENGINE & MIDDLEWARES */
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+/* SERVER */
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+/* LISTEN */
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+
