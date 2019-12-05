7c6bf84dbfbdbab920409e2669fffe5f7e4c34ca
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-12-05 16:26:56 +0100

    update files

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,5 +1,4 @@
 /* === jQUERY === */
-
 $(document).ready(function() {
 
     /* HAMBURGR ICON  */
@@ -16,6 +15,44 @@ $(document).ready(function() {
     /* CHAT BTN */
 
     /* CHAT USER INPUT */
+
+    /* FUNCTIONS */
+    const addPost = (post) => {
+        //get properties
+        const {username, type, avatar, time, msg} = post;
+
+        //create elements
+        const $posts = $('#chatroom .posts');
+        const $post = `
+            <div class="post post--${type}">
+                <img src="assets/img/${avatar}" alt="${type}" class="post-img">
+                <div class="post-body">
+                    <p class="card-text post-info">
+                        <span class="post-author">${username}</span> 
+                        <i class="far fa-clock"></i>
+                        <time class="post-time text-muted">${time}</time>
+                    </p>
+                    <p class="card-text post-msg">${msg}</p>
+                </div>
+            </div>
+        `;
+
+        //append to DOM
+        $posts.append($post);
+    }
+
+    /* EVENTS */
+    $('#send-post').click(() => {
+        const post = {
+            username: 'julio',
+            type: 'me',
+            avatar: 'man-1.jpg',
+            time: 'now', 
+            msg: $('textarea').val(),
+        }
+        addPost(post);
+        $('textarea').val('');
+    })
 
     /* SOCKET MESSAGES */
 
